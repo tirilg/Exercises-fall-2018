7db3a3790a84f3b15371055b63f6ec118b0f2d79
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-09-04 10:32:08 +0200

    the bar-chart works. exercise done

--- bar-chart/main.js
+++ bar-chart/main.js
@@ -2,54 +2,55 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
-let bars;
+let bar;
 let barsArray;
 let dataArray = [];
+let counter;
+let bars;
 
 function init() {
   console.log("init");
 
   // Get bars
-  bars = document.querySelectorAll(".bar");
+  bar = document.querySelectorAll(".bar");
 
   // Convert bars to array
-  barsArray = Array.from(bars);
-  console.log(barsArray);
+  //barsArray = Array.from(bars);
+  //console.log(barsArray);
 
   // Start the loop
   setTimeout(loop, 100);
 
   // Make a for loop
 
-  for () {
-    
+  for (counter = 0; counter < 40; counter++) {
+    dataArray.push(Math.random() * 100);
+  }
+  // run the display function
+  displayAllBars();
+
+  function displayAllBars() {
+    document.querySelectorAll(".bar").forEach(displayOneBar);
+  }
+
+  function displayOneBar(div, index, nodeList) {
+    div.style.height = dataArray[index] + "px";
   }
 }
 
 function loop() {
   //add  array in the end
-  barsArray.push(barsArray[0]);
+  //barsArray.push(barsArray[0]);
+
+  bar = document.querySelectorAll(".bar");
+  bars = document.querySelector(".myBars");
+  bars.appendChild(bar[0]);
 
   console.log(barsArray);
 
   //take one piece of the array
-  barsArray = barsArray.slice(1);
+  //barsArray = barsArray.slice(1);
 
   // Start the loop
-  setTimeout(loop, 300);
+  setTimeout(loop, 220);
 }
-
-function displayAllBars() {
-  document.querySelectorAll(".bar").forEach(displayOneBar);
-}
-
-function displayOneBar(div, index, nodeList) {
-  div.style.height = "120px";
-}
-//create data ? Math().random * 100;
-
-//we have to use .forEach
-
-//function for displaying and function for running
-
-//loop through the bars and set the height accoring to the data
